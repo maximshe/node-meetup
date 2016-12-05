e95fe7ce5e262fae80538796aa7d6137c2b0e119
Maksym Shekhovets <maximsh@interlink-ua.com>
2016-12-05 23:42:49 +0200

    added basic serve static functionality

--- hello-world.js
+++ hello-world.js
@@ -1,23 +1,37 @@
 var http = require('http');
+var fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) {
+        responseCode = 200;
+    }
+    fs.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('500 - Internal Server Error');
+        }
+        else {
+            res.writeHead(responseCode, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    })
+}
+
 http.createServer(function(req,res){
     // normalize url by removing querystring, optional
     // trailing slash, and making it lowercase
     var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
     switch(path) {
     case '':
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Homepage');
+        serveStaticFile(res, '/public/home.html', 'text/html');
         break;
     case '/about':
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('About');
+        serveStaticFile(res, '/public/about.html', 'text/html');
         break;
     default:
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
+        serveStaticFile(res, '/public/404.html', 'text/html', 404);
         break;
     }
 }).listen(3000);
 
 console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
- 
